63a35183f9eb186fc031af773eb6d44f53e6c6d7
David Skudra <David4Danger@users.noreply.github.com>
2019-06-25 19:01:54 -0400

    Added game_id values for years 2010-2016
    They're not easy to find so it is good to have them stored *somewhere*

--- league_ids.py
+++ league_ids.py
@@ -1,4 +1,11 @@
 league_ids = {
+    2010: {'game_id': 238, 'league_id': -1},
+    2011: {'game_id': 253, 'league_id': -1},
+    2012: {'game_id': 268, 'league_id': -1},
+    2013: {'game_id': 308, 'league_id': -1},
+    2014: {'game_id': 328, 'league_id': -1},
+    2015: {'game_id': 346, 'league_id': -1},
+    2016: {'game_id': 357, 'league_id': -1},
     2017: {'game_id': 370, 'league_id': 49267},
     2018: {'game_id': 378, 'league_id': 5115},
     2019: {'game_id': 388, 'league_id': 368}
